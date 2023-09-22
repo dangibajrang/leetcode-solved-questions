f3cbf7de5bf5f436f3d1198e0a037ad500b8e07f
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2023-09-22 17:06:36 +0530

    Added solution - LeetHub

---
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.java
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+
+class GFG
+{
+    int findFirst(int[] arr,int n,int x)
+    {
+        int low=0,high=n-1,firstOccur=-1;
+        
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            
+            if(arr[mid]==x)
+            {
+                firstOccur=mid; 
+                high=mid-1;
+            }
+            else if(arr[mid]<x) low=mid+1;
+            else high=mid-1;
+        }
+        
+        return firstOccur;
+    }
+    int findLast(int[] arr,int n,int x)
+    {
+        int low=0,high=n-1,lastOccur=-1;
+        
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            
+            if(arr[mid]==x)
+            {
+                lastOccur=mid; 
+                low=mid+1;
+            }
+            else if(arr[mid]<x) low=mid+1;
+            else high=mid-1;
+        }
+        
+        return lastOccur;
+    }
+    ArrayList<Integer> find(int arr[], int n, int x)
+    {
+        int first=findFirst(arr,n,x);
+        
+        if(first==-1) return new ArrayList<>(Arrays.asList(-1, -1));
+        
+        int last=findLast(arr,n,x);
+        
+        return new ArrayList(Arrays.asList(first,last));
+    }
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while (testcases-- > 0) {
+//            int n = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] q = line.trim().split("\\s+");
+            int n =Integer.parseInt(q[0]);
+            int x =Integer.parseInt(q[1]);
+//            //int y =Integer.parseInt(q[2]);
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(a1[i]);
+            }
+            GFG ob = new GFG();
+            ArrayList<Integer> ans=ob.find(arr,n,x);
+            System.out.println(ans.get(0)+" "+ans.get(1));
+        }
+    }
+}
+
+// } Driver Code Ends
