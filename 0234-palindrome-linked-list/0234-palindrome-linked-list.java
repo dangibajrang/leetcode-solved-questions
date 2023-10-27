4d2c5c545e7f8cca3e271b273d4394036783ae1c
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2023-10-27 10:22:58 +0530

    Time: 3 ms (100%), Space: 56.8 MB (54.02%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -8,49 +8,80 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+//  1st approach
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+
+//          //base case 
+//         if(head == null || head.next == null) return true;
+        
+//         //define slow and fast
+//         ListNode slow = head, fast = head;
+        
+//         //move slow and fast in the right position
+//         while(fast != null && fast.next != null){
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+        
+//         //reverse the last half of the list
+//         ListNode L = head, R = reverse(slow);
+        
+//         //check if is palindrome
+//         while(R != null){
+//             if(L.val == R.val){
+//                 L = L.next;
+//                 R = R.next;
+//             }else{ 
+//                 return false;
+//             }
+//         }
+        
+//         return true;
+//     }
+//     private ListNode reverse(ListNode head){
+//         // base case
+//         if(head == null || head.next == null) return head;
+        
+//         //reverse
+//         ListNode pre = null, cur = head;
+//         while(cur != null){
+//             ListNode temp = cur.next;
+//             cur.next = pre;
+//             pre = cur;
+//             cur = temp;
+//         }
+//         return pre;
+        
+//     }
+// }
+
+
+// 2nd approach
 class Solution {
     public boolean isPalindrome(ListNode head) {
-
-         //base case 
-        if(head == null || head.next == null) return true;
-        
-        //define slow and fast
-        ListNode slow = head, fast = head;
-        
-        //move slow and fast in the right position
-        while(fast != null && fast.next != null){
+        ListNode slow = head, fast = head, prev, temp;
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-        
-        //reverse the last half of the list
-        ListNode L = head, R = reverse(slow);
-        
-        //check if is palindrome
-        while(R != null){
-            if(L.val == R.val){
-                L = L.next;
-                R = R.next;
-            }else{ 
-                return false;
-            }
+        prev = slow;
+        slow = slow.next;
+        prev.next = null;
+        while (slow != null) {
+            temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = temp;
         }
-        
+        fast = head;
+        slow = prev;
+        while (slow != null) {
+            if (fast.val != slow.val) return false;
+            fast = fast.next;
+            slow = slow.next;
+        }
         return true;
-    }
-    private ListNode reverse(ListNode head){
-        // base case
-        if(head == null || head.next == null) return head;
-        
-        //reverse
-        ListNode pre = null, cur = head;
-        while(cur != null){
-            ListNode temp = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = temp;
-        }
-        return pre;
-        
     }
 }
