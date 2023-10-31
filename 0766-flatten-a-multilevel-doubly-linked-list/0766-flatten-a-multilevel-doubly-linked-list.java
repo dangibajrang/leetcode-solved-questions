93fa58bbbfe204d5fffec607484f3cbc2c11a219
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2023-10-31 18:54:18 +0530

    Time: 0 ms (100%), Space: 40.6 MB (39.63%) - LeetHub

---
+++ 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.java
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    public Node flatten(Node head) {
+        dfs(head);
+        return head;
+    }
+    private Node dfs(Node node){
+        Node pre = null;
+        while(node != null){
+            pre = node;
+            if(node.child != null){
+                //Change the tail node
+                Node tail = dfs(node.child);
+                tail.next = node.next;
+                if(node.next != null){
+                    node.next.prev = tail;
+                }
+                
+                //Change the head node
+                node.next = node.child;
+                node.child.prev = node;
+                node.child = null;
+                
+                //Reset the pointers
+                node = tail.next;
+                pre = tail;
+            }else{
+                node = node.next;
+            }
+        }
+        return pre;
+    }
+}
