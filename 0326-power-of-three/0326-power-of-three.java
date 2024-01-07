8a86fd99061ddc05afbfff29edcfaaf363601e8e
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2024-01-07 20:55:26 +0530

    Time: 8 ms (84.78%), Space: 43.4 MB (42.95%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,11 +1,19 @@
+// class Solution {
+//     public boolean isPowerOfThree(int n) {
+        
+//         while(n>=3){
+//             if(n%3 != 0) return false;
+//             n = n/3;
+//         }
+//         return  n==1;
+        
+//     }
+// }
+
 class Solution {
     public boolean isPowerOfThree(int n) {
-        
-        while(n>=3){
-            if(n%3 != 0) return false;
-            n = n/3;
-        }
-        return  n==1;
-        
+         if(n==1) return true;
+        if(n%3!=0 || n==0) return false;
+        return isPowerOfThree(n/3);
     }
 }
