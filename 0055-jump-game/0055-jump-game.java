6f0d5ba0a858de5a6de941524ca6892c423f4f19
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2024-01-24 20:06:51 +0530

    Time: 3 ms (55.86%), Space: 45.8 MB (24.26%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,27 +1,11 @@
 class Solution {
-    Boolean[] cache;
-    int[] nums;
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        this.nums = nums;
-        this.cache = new Boolean[n];
-        return dfs(0);
+         int reachable = 0;
+    for (int i=0; i<nums.length; ++i) {
+        if (i > reachable) return false;
+        reachable = Math.max(reachable, i + nums[i]);
     }
-    private boolean dfs(int index){
-        //Base case
-        if(nums.length - 1 <= index) return true;
+    return true;
         
-        if(cache[index] != null) return cache[index];
-        
-        int maxJump = nums[index];
-        
-        for(int i = 1; i <= maxJump; i++){
-            boolean res = dfs(index + i);
-            if(res){
-                cache[index] = true;
-                return cache[index];
-            }
-        }
-        return cache[index] = false;
     }
 }
