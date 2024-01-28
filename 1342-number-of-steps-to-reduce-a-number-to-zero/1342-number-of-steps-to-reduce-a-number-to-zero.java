d1f59ba67081e4a4b002f83afae2f2fe028c9d6d
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2024-01-28 19:12:03 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (62.05%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,9 +1,24 @@
+// class Solution {
+//     public int numberOfSteps(int num) {
+//         if (num == 0)
+//       return 0;
+//     final int subtractSteps = Integer.bitCount(num);
+//     final int divideSteps = 31 - Integer.numberOfLeadingZeros(num);
+//     return subtractSteps + divideSteps;
+//     }
+// }
+
 class Solution {
     public int numberOfSteps(int num) {
-        if (num == 0)
-      return 0;
-    final int subtractSteps = Integer.bitCount(num);
-    final int divideSteps = 31 - Integer.numberOfLeadingZeros(num);
-    return subtractSteps + divideSteps;
+        return helper(num,0);
+    }
+    private int helper(int num, int steps){
+        if(num == 0){
+            return steps;
+        }
+        if(num % 2 == 0){
+            return helper(num/2 , steps +1);
+        }
+        return helper(num-1 ,steps +1);
     }
 }
