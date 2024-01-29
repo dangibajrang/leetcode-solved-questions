84e6d6ceb9d4ebd6536834b90c5ec1310b78b565
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2024-01-29 08:39:55 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (5.57%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -0,0 +1,75 @@
+// import java.util.*;
+
+// class MyQueue {
+//     Stack<Integer> pushStack = new Stack<>();
+//     Stack<Integer> popStack = new Stack<>();
+
+//     /** Initialize your data structure here. */
+//     public MyQueue() {
+
+//     }
+
+//     /** Push element x to the back of queue. */
+//     public void push(int x) {
+//         pushStack.add(x);
+//     }
+
+//     /** Removes the element from in front of queue and returns that element. */
+//     public int pop() {
+//         transferItems();
+//         return popStack.pop();
+//     }
+
+//     private void transferItems() {
+//         if (popStack.isEmpty()) {
+//             // Transfer elements onto the pop stack
+//             while (!pushStack.isEmpty()) {
+//                 popStack.add(pushStack.pop());
+//             }
+//         }
+//     }
+
+//     /** Get the front element. */
+//     public int peek() {
+//         transferItems();
+//         return popStack.peek();
+//     }
+
+//     /** Returns whether the queue is empty. */
+//     public boolean empty() {
+//         return pushStack.isEmpty() && popStack.isEmpty();
+//     }
+// }
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+
+class MyQueue {
+  public void push(int x) {
+    input.push(x);
+  }
+
+  public int pop() {
+    peek();
+    return output.pop();
+  }
+
+  public int peek() {
+    if (output.isEmpty())
+      while (!input.isEmpty())
+        output.push(input.pop());
+    return output.peek();
+  }
+
+  public boolean empty() {
+    return input.isEmpty() && output.isEmpty();
+  }
+
+  private Deque<Integer> input = new ArrayDeque<>();
+  private Deque<Integer> output = new ArrayDeque<>();
+}
