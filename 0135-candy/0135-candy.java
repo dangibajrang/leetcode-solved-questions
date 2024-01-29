60d9541df1caeeeca38a2d7f3ba371d1c0150137
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2024-01-29 18:34:42 +0530

    Time: 4 ms (40.72%), Space: 45.2 MB (64.92%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -8,7 +8,7 @@ class Solution {
     Arrays.fill(l, 1);
     Arrays.fill(r, 1);
 
-    for (int i = 1; i < n; ++i)
+    for (int i = 1; i < n; i++)
       if (ratings[i] > ratings[i - 1])
         l[i] = l[i - 1] + 1;
 
@@ -16,7 +16,7 @@ class Solution {
       if (ratings[i] > ratings[i + 1])
         r[i] = r[i + 1] + 1;
 
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < n; i++)
       ans += Math.max(l[i], r[i]);
 
     return ans;
