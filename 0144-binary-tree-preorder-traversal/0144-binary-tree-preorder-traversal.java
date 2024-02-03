99c1b8988bdcd5ec859bbe9e62fd57158c00e545
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2024-02-03 11:21:16 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (15.38%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -13,28 +13,48 @@
  *     }
  * }
  */
-class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-          List<Integer> res = new LinkedList<>();
-        if(root == null) return res;
-        Stack<TreeNode> stack = new Stack<>();
+// class Solution {
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//           List<Integer> res = new LinkedList<>();
+//         if(root == null) return res;
+//         Stack<TreeNode> stack = new Stack<>();
 
-       stack.add(root);
+//        stack.add(root);
 
-        while(stack.isEmpty() == false){
-            TreeNode top = stack.pop();
-            //add the current node 
-            res.add(top.val);
-            if(top.right != null){
-                //ensure that right is at the bottom of the left
-                stack.add(top.right);
-            }
-            if(top.left != null){
-                //ensure that left is on the top
-                stack.add(top.left);
-            }
-        }
-        return res;
+//         while(stack.isEmpty() == false){
+//             TreeNode top = stack.pop();
+//             //add the current node 
+//             res.add(top.val);
+//             if(top.right != null){
+//                 //ensure that right is at the bottom of the left
+//                 stack.add(top.right);
+//             }
+//             if(top.left != null){
+//                 //ensure that left is on the top
+//                 stack.add(top.left);
+//             }
+//         }
+//         return res;
         
-    }
+//     }
+// }
+
+
+
+// recursive
+class Solution {
+  public List<Integer> preorderTraversal(TreeNode root) {
+    List<Integer> ans = new ArrayList<>();
+    preorder(root, ans);
+    return ans;
+  }
+
+  private void preorder(TreeNode root, List<Integer> ans) {
+    if (root == null)
+      return;
+
+    ans.add(root.val);
+    preorder(root.left, ans);
+    preorder(root.right, ans);
+  }
 }
