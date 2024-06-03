c11bc39909153a20c2fdfa6d63dcd44a7f92ca60
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2024-06-03 07:27:52 +0530

    Time: 0 ms (100%), Space: 40.2 MB (53.01%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,25 @@
+// class Solution {
+//     public int fib(int n) {
+//         if (n < 2) return n;
+         
+//         return fib(n-1)+ fib(n-2);
+        
+//     }
+// }
+
+
 class Solution {
     public int fib(int n) {
-        if (n < 2)
-      return n;
-            else {
-                return fib(n-1)+ fib(n-2);
-         }
+        if(n<=1) return n;
+        int seclast = 0, last = 1;
+        int ans = 0;
+
+        for(int i = 2; i<=n; i++) {
+            ans = seclast + last;
+            seclast = last;
+            last = ans;
+        }
+
+        return ans;
     }
 }
