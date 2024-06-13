57883a4909ed1cb221f696791726e63362864b22
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2024-06-13 07:55:01 +0530

    Time: 8 ms (52.68%), Space: 44.4 MB (94.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,41 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] ans = new int[2];
-        for(int i =0 ; i<nums.length; i++){
-            for(int j =i+1 ; j<nums.length; j++){
-                if(nums[i] + nums[j] ==  target){
-                    ans[0] =i;
-                    ans[1] = j;
-                    break;
-                }
-            }
-            
+    public int[] twoSum(int[] numbers, int target) {
+           int left = 0 , n =numbers.length, right =n-1;
+             int[][] numsWithIndices = new int[numbers.length][2];
+        for (int i = 0; i < numbers.length; i++) {
+            numsWithIndices[i][0] = numbers[i];
+            numsWithIndices[i][1] = i;
         }
-        return ans;
+
+
+             Arrays.sort(numsWithIndices, (a, b) -> Integer.compare(a[0], b[0]));
+        // while(L<R){
+        //     int sum = numbers[L]+numbers[R];
+        //     if(sum ==  target){
+        //         return new int[] {L, R};
+
+        //     }else if(sum<target){
+        //         L++;
+        //     }else{
+        //         R--;
+        //     }
+                   
+        //  }
+             while (left < right) {
+            int leftNum = numsWithIndices[left][0];
+            int rightNum = numsWithIndices[right][0];
+            int currentSum = leftNum + rightNum;
+
+            if (currentSum == target) {
+                return new int[] { numsWithIndices[left][1], numsWithIndices[right][1] };
+            } else if (currentSum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+         return new int[] {-1,-1};
+        
     }
 }
