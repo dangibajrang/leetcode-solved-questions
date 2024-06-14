30f9e0cc657371121de2592d7d359fdeca06e59f
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2024-06-14 17:20:35 +0530

    Time: 38 ms (43.61%), Space: 60.2 MB (44.92%) - LeetHub

---
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minIncrementForUnique(int[] nums) {
+        int ans = 0;
+        int minAvailable = 0;
+
+        Arrays.sort(nums);
+
+        for (final int num : nums) {
+        ans += Math.max(minAvailable - num, 0);
+        minAvailable = Math.max(minAvailable, num) + 1;
+        }
+
+        return ans;
+        
+    }
+}
