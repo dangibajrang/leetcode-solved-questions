1cf0376d75f04518dc5bb5342146215fa1d0d3b6
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2024-08-05 20:16:38 +0530

    Time: 121 ms (39.88%), Space: 46.2 MB (15.65%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int start = 0; // Start index of the longest palindromic substring
+        int maxLength = 1; // Length of the longest palindromic substring
+
+        // All substrings of length 1 are palindromes
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        // Check for substrings of length 2
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                start = i;
+                maxLength = 2;
+            }
+        }
+
+        // Check for substrings of length greater than 2
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i < n - len + 1; i++) {
+                int j = i + len - 1;
+                // Check if the substring s[i...j] is a palindrome
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    start = i;
+                    maxLength = len;
+                }
+            }
+        }
+
+        return s.substring(start, start + maxLength);
+    }
+
+}
