4ff60c58594a42ef5390e7381847564c4f3ec2b8
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2024-08-29 10:28:50 +0530

    Time: 1 ms (87.28%), Space: 41.4 MB (10.12%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -1,15 +1,39 @@
-class Solution {
-    public int numIdenticalPairs(int[] nums) {
-         int count = 0;
-        int[] frequency = new int[101]; // since 1 <= nums[i] <= 100
+// class Solution {
+//     public int numIdenticalPairs(int[] nums) {
+//          int count = 0;
+//         int[] frequency = new int[101]; // since 1 <= nums[i] <= 100
 
-        for (int num : nums) {
-            count += frequency[num];
-            frequency[num]++;
-        }
+//         for (int num : nums) {
+//             count += frequency[num];
+//             frequency[num]++;
+//         }
 
-        return count;
+//         return count;
     
         
+//     }
+// }
+
+class Solution {
+
+    public int numIdenticalPairs(int[] nums) {
+        // Initialize a map to store the frequency of each number
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+
+        // Count the frequency of each number
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        // Initialize the count of good pairs
+        int goodPairs = 0;
+
+        // Calculate the number of good pairs
+        for (int count : frequencyMap.values()) {
+            goodPairs += count * (count - 1) / 2;
+        }
+
+        // Return the total number of good pairs
+        return goodPairs;
     }
 }
