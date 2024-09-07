ddab32143a5419a47b57d187a83fe84ba7af9a06
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2024-09-07 10:59:56 +0530

    Time: 3 ms (67.35%), Space: 45.9 MB (5.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,41 +1,13 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-           int left = 0 , n =numbers.length, right =n-1;
-             int[][] numsWithIndices = new int[numbers.length][2];
-        for (int i = 0; i < numbers.length; i++) {
-            numsWithIndices[i][0] = numbers[i];
-            numsWithIndices[i][1] = i;
-        }
-
-
-             Arrays.sort(numsWithIndices, (a, b) -> Integer.compare(a[0], b[0]));
-        // while(L<R){
-        //     int sum = numbers[L]+numbers[R];
-        //     if(sum ==  target){
-        //         return new int[] {L, R};
-
-        //     }else if(sum<target){
-        //         L++;
-        //     }else{
-        //         R--;
-        //     }
-                   
-        //  }
-             while (left < right) {
-            int leftNum = numsWithIndices[left][0];
-            int rightNum = numsWithIndices[right][0];
-            int currentSum = leftNum + rightNum;
-
-            if (currentSum == target) {
-                return new int[] { numsWithIndices[left][1], numsWithIndices[right][1] };
-            } else if (currentSum < target) {
-                left++;
-            } else {
-                right--;
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                return new int[]{i,map.get(target - nums[i])};
             }
+            map.put(nums[i],i);
         }
-
-         return new int[] {-1,-1};
+        return new int[0];
         
     }
 }
