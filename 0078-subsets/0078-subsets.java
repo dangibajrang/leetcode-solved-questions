979a1e44275e34020d498070c43fe8499873e730
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2024-09-25 07:36:17 +0530

    Time: 1 ms (53.44%), Space: 42.9 MB (12.25%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,24 +1,47 @@
-class Solution {
+// class Solution {
 
-    public void helper(int[] nums, int ind, List<Integer> curr, List<List<Integer>> res) {
-        if(ind == nums.length) {
-            res.add(new ArrayList<> (curr));
-            return;
+//     public void helper(int[] nums, int ind, List<Integer> curr, List<List<Integer>> res) {
+//         if(ind == nums.length) {
+//             res.add(new ArrayList<> (curr));
+//             return;
+//         }
+
+//         //inc
+//         curr.add(nums[ind]);
+//         helper(nums, ind+1, curr, res);
+//         curr.remove(curr.size() - 1);
+
+//         //exc
+//         helper(nums, ind+1, curr, res);
+//     }
+
+//     public List<List<Integer>> subsets(int[] nums) {
+//         List<Integer> curr = new ArrayList<>();
+//         List<List<Integer>> res = new ArrayList<>();
+//         helper(nums, 0, curr, res);
+//         return res;
+//     }
+// }
+
+
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> result = new ArrayList<>();
+    backtrack(result, new ArrayList<>(), nums, 0);
+    return result;
+}
+
+        private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums, int start) {
+            result.add(new ArrayList<>(tempList)); // Add current subset
+            for (int i = start; i < nums.length; i++) {
+                tempList.add(nums[i]);
+                backtrack(result, tempList, nums, i + 1);
+                tempList.remove(tempList.size() - 1);  // Backtrack
+            }
         }
 
-        //inc
-        curr.add(nums[ind]);
-        helper(nums, ind+1, curr, res);
-        curr.remove(curr.size() - 1);
-
-        //exc
-        helper(nums, ind+1, curr, res);
-    }
-
-    public List<List<Integer>> subsets(int[] nums) {
-        List<Integer> curr = new ArrayList<>();
-        List<List<Integer>> res = new ArrayList<>();
-        helper(nums, 0, curr, res);
-        return res;
-    }
 }
+
+
+
